73387c65ac4faacb90ac5382677eb969a7b2d964
Cabe <Cabe@drgn.net>
2026-08-19 17:00:37 -0400

    Add battery support to watch screen

--- include/iris/screens/WatchScreen.h
+++ include/iris/screens/WatchScreen.h
@@ -1,13 +1,15 @@
 #pragma once
 
 #include "iris/screens/Screen.h"
+#include "iris/services/BatteryService.h"
 #include "iris/services/TimeService.h"
 
 namespace iris {
 
 class WatchScreen : public Screen {
  public:
-  explicit WatchScreen(TimeService& timeService) : timeService_(timeService) {}
+  WatchScreen(TimeService& timeService, BatteryService& battery)
+      : timeService_(timeService), battery_(battery) {}
 
   void enter() override;
   void update(uint32_t nowMs) override;
@@ -17,8 +19,13 @@ class WatchScreen : public Screen {
   void onButtonB() override;
 
  private:
+  void drawStaticLayout();
+  void drawBattery();
+
   TimeService& timeService_;
+  BatteryService& battery_;
   uint32_t lastDrawMs_ = 0;
+  bool layoutDrawn_ = false;
 };
 
 }  // namespace iris
