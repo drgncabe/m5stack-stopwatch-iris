660d6c94de663cff25e9ee66a657ef7f99b0330f
Cabe <Cabe@drgn.net>
2026-08-19 11:28:47 -0400

    Fix AppConfig constants for default C++ mode

--- include/iris/AppConfig.h
+++ include/iris/AppConfig.h
@@ -4,19 +4,19 @@
 
 namespace iris::config {
 
-inline constexpr char kProjectName[] = "Iris";
-inline constexpr char kVersion[] = "0.1.0-phase1";
+constexpr char kProjectName[] = "Iris";
+constexpr char kVersion[] = "0.1.0-phase1";
 
 // POSIX timezone string for US Eastern time. Change this in later deployments
 // if the device should operate in another timezone.
-inline constexpr char kTimezone[] = "EST5EDT,M3.2.0,M11.1.0";
-inline constexpr char kNtpServer1[] = "0.pool.ntp.org";
-inline constexpr char kNtpServer2[] = "1.pool.ntp.org";
-inline constexpr char kNtpServer3[] = "2.pool.ntp.org";
+constexpr char kTimezone[] = "EST5EDT,M3.2.0,M11.1.0";
+constexpr char kNtpServer1[] = "0.pool.ntp.org";
+constexpr char kNtpServer2[] = "1.pool.ntp.org";
+constexpr char kNtpServer3[] = "2.pool.ntp.org";
 
-inline constexpr uint8_t kDefaultVolume = 96;
-inline constexpr uint32_t kWifiReconnectMs = 15000;
-inline constexpr uint32_t kNtpRetryMs = 30000;
-inline constexpr uint32_t kNtpResyncMs = 6UL * 60UL * 60UL * 1000UL;
+constexpr uint8_t kDefaultVolume = 96;
+constexpr uint32_t kWifiReconnectMs = 15000;
+constexpr uint32_t kNtpRetryMs = 30000;
+constexpr uint32_t kNtpResyncMs = 6UL * 60UL * 60UL * 1000UL;
 
 }  // namespace iris::config
