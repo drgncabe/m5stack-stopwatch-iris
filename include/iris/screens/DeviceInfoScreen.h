ccb09af6a7b0d46416c051a2f74120b3fd99af58
Cabe <Cabe@drgn.net>
2026-08-19 17:00:38 -0400

    Add battery status to device info screen

--- include/iris/screens/DeviceInfoScreen.h
+++ include/iris/screens/DeviceInfoScreen.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "iris/screens/Screen.h"
+#include "iris/services/BatteryService.h"
 #include "iris/services/TimeService.h"
 #include "iris/services/WifiService.h"
 
@@ -8,8 +9,8 @@ namespace iris {
 
 class DeviceInfoScreen : public Screen {
  public:
-  DeviceInfoScreen(WifiService& wifi, TimeService& timeService)
-      : wifi_(wifi), timeService_(timeService) {}
+  DeviceInfoScreen(WifiService& wifi, TimeService& timeService, BatteryService& battery)
+      : wifi_(wifi), timeService_(timeService), battery_(battery) {}
 
   void enter() override;
   void update(uint32_t nowMs) override;
@@ -23,6 +24,7 @@ class DeviceInfoScreen : public Screen {
 
   WifiService& wifi_;
   TimeService& timeService_;
+  BatteryService& battery_;
 };
 
 }  // namespace iris
