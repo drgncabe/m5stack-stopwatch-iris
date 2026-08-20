0c49aed6988584739a5e0ae59ca1a5487c752cdd
Cabe <Cabe@drgn.net>
2026-08-20 09:15:28 -0400

    Store watch background preference

--- src/services/SettingsStore.cpp
+++ src/services/SettingsStore.cpp
@@ -7,6 +7,7 @@ void SettingsStore::begin() {
   prefs_.begin("iris", false);
   volume_ = prefs_.getUChar("volume", config::kDefaultVolume);
   wifiEnabled_ = prefs_.getBool("wifi_on", true);
+  watchBackground_ = prefs_.getUChar("watch_bg", 0);
 }
 
 void SettingsStore::setVolume(uint8_t value) {
@@ -17,6 +18,11 @@ void SettingsStore::setVolume(uint8_t value) {
 void SettingsStore::setWifiEnabled(bool enabled) {
   wifiEnabled_ = enabled;
   prefs_.putBool("wifi_on", wifiEnabled_);
+}
+
+void SettingsStore::setWatchBackground(uint8_t value) {
+  watchBackground_ = value;
+  prefs_.putUChar("watch_bg", watchBackground_);
 }
 
 }  // namespace iris
