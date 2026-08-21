3a828a451cb1c889dd00dff51715851203dbbd76
Cabe <Cabe@drgn.net>
2026-08-20 23:37:18 -0400

    Restore theme widgets screen header

--- include/iris/screens/BackgroundScreen.h
+++ include/iris/screens/BackgroundScreen.h
@@ -12,16 +12,24 @@ class BackgroundScreen : public Screen {
   void enter() override;
   void update(uint32_t nowMs) override;
   void draw() override;
+  void previewTouch(int32_t x, int32_t y) override;
   void handleTouch(int32_t x, int32_t y) override;
   void onButtonA() override;
   void onButtonB() override;
 
  private:
-  void nextBackground();
+  void activateSelected();
+  void selectRow(size_t index);
+  void drawRow(size_t index, bool selected);
+  int rowAt(int32_t x, int32_t y) const;
+  void nextTheme();
+  void toggleWidget(uint8_t widget);
   void goBack();
-  const char* backgroundName() const;
+  const char* rowLabel(size_t index) const;
+  String rowValue(size_t index) const;
 
   SettingsStore& settings_;
+  size_t selected_ = 0;
 };
 
 }  // namespace iris
