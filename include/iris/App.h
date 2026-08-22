28eaee55835b49b61b92d5474502d9a9725dbb27
Cabe <Cabe@drgn.net>
2026-08-22 10:02:23 -0400

    Wire axis calibration screen into app

--- include/iris/App.h
+++ include/iris/App.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "iris/screens/AxisCalibrationScreen.h"
 #include "iris/screens/BootloaderScreen.h"
 #include "iris/screens/BackgroundScreen.h"
 #include "iris/screens/DeviceInfoScreen.h"
@@ -75,11 +76,13 @@ class App {
   SpinnerFidgetScreen spinnerFidgetScreen_;
   GravityBallFidgetScreen gravityBallFidgetScreen_;
   MenuScreen developerMenuScreen_;
+  AxisCalibrationScreen axisCalibrationScreen_;
   BootloaderScreen bootloaderScreen_;
   DeviceInfoScreen deviceInfoScreen_;
   bool touchActive_ = false;
   bool touchPreviewed_ = false;
   bool touchHandled_ = false;
+  bool touchMoved_ = false;
   int32_t touchStartX_ = 0;
   int32_t touchStartY_ = 0;
   uint32_t touchStartMs_ = 0;
